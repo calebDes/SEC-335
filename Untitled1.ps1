05c452a4f1fd691d5366d042ca773ba25da78178
calebDes <58434225+calebDes@users.noreply.github.com>
2022-02-17 19:44:18 -0500

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,13 @@
+﻿param($network, $server)
+
+for($i=1; $i -le 254; $i++){
+   $name = Resolve-DnsName -DnsOnly "$network.$i" -Server $server -ErrorAction Ignore | Select-Object NameHost
+   if ($name -eq $null){
+   }
+   else {
+     echo "$network.$i $name"
+   }
+}
+#figure out how to extract name host: $name.namehost
+#if name host isn't found then make sure it isn't outputted
+
